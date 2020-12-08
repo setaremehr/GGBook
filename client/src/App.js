0bb296de089316151d09f021567489553c04a0f1
setare mehr <setaremehr89@gmail.com>
2020-12-08 12:55:37 -0800

    add saved component to the routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 import { Container} from "./components/Grid";
 
 import Search from './pages/Search';
-
+import Saved from "./pages/Saved";
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
+    <div className="App">
+      <header className="App-header">
         <Router>
           <Container>
             <Switch>
             <Route exact path="/" component={Search} />
-        
+            <Route path="/bookshelf" component={Saved} />
             </Switch>
           </Container>
         </Router>
-    //   </header>
-    // </div>
+       </header>
+   </div> 
   );
 }
 
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -3,92 +3,95 @@ import axios from "axios";
 import AddBookBtn from "../../components/AddBookBtn";
 import { Row, Col } from "../../components/Grid";
 import { BookList, BookListItem } from "../../components/BookList";
-
+import EmptyList from "../../components/EmptyList";
 
 
 class Search extends Component {
-    state = {
-        searchRes: [],
-        query: "",
-        books: []
-    };
+  state = {
+    searchRes: [],
+    query: "",
+    books: []
+  };
 
-    displayRes = data => {
-        this.setState({ books: data.items });
-    };
+  displayRes = data => {
+    this.setState({ books: data.items });
+  };
 
-    searchGBooks = () => {
-        let url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.query
-            }`;
-        axios
-            .get(url)
-            .then(res => {
-                //console.log(res);
-                this.displayRes(res.data);
-            })
-            .catch(err => console.log(err));
-    };
+  searchGBooks = () => {
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${
+      this.state.query
+    }`;
+    axios
+      .get(url)
+      .then(res => {
+        //console.log(res);
+        this.displayRes(res.data);
+      })
+      .catch(err => console.log(err));
+  };
 
-    handleInput = event => {
-        const { name, value } = event.target;
+  handleInput = event => {
+    const { name, value } = event.target;
 
-        this.setState({
-            [name]: value
-        });
-        //console.log("Query", this.state.query);
-    };
-
-
+    this.setState({
+      [name]: value
+    });
+    //console.log("Query", this.state.query);
+  };
 
 
-    render() {
-        return (
-            <Row>
-                <Col size="md-12">
-                    <div>
-                        <input id="bookQ" className="form-control form-control-lg" autoComplete="off" type="text" name="query" onChange={this.handleInput} />
-                        <button type="submit" onClick={this.searchGBooks} >
-                            Search for Books
+  
+
+  render() {
+    return (
+      <Row>
+        <Col size="md-12">
+        <div>
+          <input id="bookQ" className="form-control form-control-lg" autoComplete="off" type="text" name="query" onChange={this.handleInput} />
+          <button  type="submit" onClick={this.searchGBooks} >
+            Search for Books
           </button>
+                   
 
+          {(this.state.books && this.state.books.length > 0) ? 
+          <BookList>
+          {this.state.books.map(book => {
+            return (
+              <div>
+              <BookListItem
+              key={book.id} 
+              authors={book.volumeInfo.authors ? book.volumeInfo.authors : ["No Author Available"]}
+              title={book.volumeInfo.title}
+              synopsis={book.volumeInfo.description ? 
+                book.volumeInfo.description : "No Description Available"}
+              link={book.volumeInfo.infoLink}
+              thumbnail={book.volumeInfo.imageLinks.thumbnail ? 
+                book.volumeInfo.imageLinks.thumbnail : "#"}
+              />
 
-                        
-                            <BookList>
-                                {this.state.books.map(book => {
-                                    return (
-                                        <div>
-                                            <BookListItem
-                                                key={book.id}
-                                                authors={book.volumeInfo.authors ? book.volumeInfo.authors : ["No Author Available"]}
-                                                title={book.volumeInfo.title}
-                                                synopsis={book.volumeInfo.description ?
-                                                    book.volumeInfo.description : "No Description Available"}
-                                                link={book.volumeInfo.infoLink}
-                                                thumbnail={book.volumeInfo.imageLinks.thumbnail ?
-                                                    book.volumeInfo.imageLinks.thumbnail : "#"}
-                                            />
-
-                                            <AddBookBtn
-                                                authors={book.volumeInfo.authors ? book.volumeInfo.authors : ["No Author Available"]}
-                                                title={book.volumeInfo.title}
-                                                synopsis={book.volumeInfo.description ?
-                                                    book.volumeInfo.description : "No Description Available"}
-                                                link={book.volumeInfo.infoLink}
-                                                thumbnail={book.volumeInfo.imageLinks.thumbnail ?
-                                                    book.volumeInfo.imageLinks.thumbnail : "#"}
-
-                                            />
-                                        </div>
-                                    )
-                                })}
-                            </BookList>
-                          
-
-                    </div>
-                </Col>
-            </Row>
-        );
-    }
+              <AddBookBtn
+              authors={book.volumeInfo.authors ? book.volumeInfo.authors : ["No Author Available"]}
+              title={book.volumeInfo.title}
+              synopsis={book.volumeInfo.description ? 
+                book.volumeInfo.description : "No Description Available"}
+              link={book.volumeInfo.infoLink}
+              thumbnail={book.volumeInfo.imageLinks.thumbnail ? 
+                book.volumeInfo.imageLinks.thumbnail : "#"}
+              
+              />
+              </div>
+            )
+          })}
+          </BookList>
+          : 
+          <EmptyList/>         
+          }
+          
+        </div>
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default Search;
