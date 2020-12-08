44f3757b2bdc73df100ff2168d86e1c49056cf30
setare mehr <setaremehr89@gmail.com>
2020-12-08 15:35:17 -0800

    add grid nav jumbotron components

---
+++ client/src/components/Grid/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function Container({ fluid, children }) {
+  return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+
+// This Row component lets us use a bootstrap row without having to think about class names
+export function Row({ fluid, children }) {
+  return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+}
+export function Col({ size, children }) {
+  return (
+    <div
+      className={size
+        .split(" ")
+        .map(size => "col-" + size)
+        .join(" ")}
+    >
+      {children}
+    </div>
+  );
+}
---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css"
+
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center"style={{height:'20px'}}>
+      
+        <div>
+          <a className="white" target="_blank" rel="noopener noreferrer" href="http://developers.google.com/books/">
+            Powered by Google Books
+          </a>
+        </div>
+      
+    </div>
+  );
+}
+
+
+
+export default Jumbotron;
