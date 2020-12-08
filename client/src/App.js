47831d968a1a82841798dcafd370682487d2a755
setare mehr <setaremehr89@gmail.com>
2020-12-07 23:26:18 -0800

    create search page and fetch googleapis

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import { Container} from "./components/Grid";
+
+import Search from './pages/Search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+        <Router>
+          <Container>
+            <Switch>
+            <Route exact path="/" component={Search} />
+        
+            </Switch>
+          </Container>
+        </Router>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ client/src/pages/Search/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+
+
+
+
+class Search extends Component {
+  state = {
+    searchRes: [],
+    query: "",
+    books: []
+  };
+
+  displayRes = data => {
+    this.setState({ books: data.items });
+  };
+
+  searchGBooks = () => {
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${
+      this.state.query
+    }`;
+    axios
+      .get(url)
+      .then(res => {
+        //console.log(res);
+        this.displayRes(res.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  handleInput = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+    //console.log("Query", this.state.query);
+  };
+
+
+  
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Search;
